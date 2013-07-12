91de42fb6b1c4ad775eeffdfffb833cb39ffe171
joao.rochamelo1993@gmail.com <joao.rochamelo1993@gmail.com@d6a40b73-17b3-d587-71e3-4df09bc2e9da>
2013-07-12 20:12:42 +0000

    Projeto adicionado

---
+++ trunk/js/barousel.js
@@ -0,0 +1,13 @@
+$("#foo3").carouFredSel({
+    items       : 1,
+    direction   : "up",
+    auto : {
+        easing      : "elastic",
+        duration    : 1000,
+        timeoutDuration: 2000,
+        pauseOnHover: true
+    }
+}).find(".slide").hover(
+    function() { $(this).find("div").slideDown(); },
+    function() { $(this).find("div").slideUp(); }
+);
