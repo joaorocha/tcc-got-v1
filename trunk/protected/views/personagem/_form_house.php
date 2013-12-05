1f2d39665d86130ffeba82bc9f4ff26fa350fad5
joao.rochamelo1993@gmail.com <joao.rochamelo1993@gmail.com@d6a40b73-17b3-d587-71e3-4df09bc2e9da>
2013-12-05 18:12:02 +0000

    Alteração dos tooltips

--- trunk/protected/views/personagem/_form_house.php
+++ trunk/protected/views/personagem/_form_house.php
@@ -21,21 +21,24 @@
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/1.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Baratheon, atualmente em posse do trono, possui um forte exercito progido
+									atras da forte muralha de Porto Real.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>1,'uncheckValue'=>null)); ?></label>
 					</td>
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/2.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Stark, em grande maioria ao Norte de Westeros, possui um grande exercito,
+									com soldados que possuem grande resistência a climas frios.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>2,'uncheckValue'=>null)); ?></label>
 					</td>
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/3.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Lannister muito conhecida pela sua riqueza, juntamente com o conjuge entre os Baratheons
+									possuem um lugar de grande escala dentro de Porto Real.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>3,'uncheckValue'=>null)); ?></label>
 					</td>
@@ -44,21 +47,25 @@
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/4.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Targaryen é famosa por seu poderio com dragões, e atualmente pelo seu exercito crescente
+									liderados pela última Targaryen Daenerys.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>4,'uncheckValue'=>null)); ?></label>
 					</td>
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/5.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Greyjoy, possui grande influência e poderio com exercito marítmo e soldados muito habilidosos
+									e astutos.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>5,'uncheckValue'=>null)); ?></label>
 					</td>
 					<td>
 						<label><a class="dcontexto">
 							<img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/web/img/casa/6.png" width="60" height="60">
-							<span>A casa Targaryen é famosa por seu poderio com dragões ebla blau poderio com dragões ebla blau poderio com dragões ebla bla</span>
+							<span>A casa Tyrell, conhecida pela vasta riqueza de recursos e exercitos explendidos, juntamente com seu
+									novo conjuge em Porto Real poderão sobreviver mais ao inverno que esta 
+									por vir.</span>
 						</a><br>
 						<?php echo $form->radioButton($model, 'id_casa', array('value'=>6,'uncheckValue'=>null)); ?></label>
 					</td>
--- trunk/protected/views/personagem/_form_mentor.php
+++ trunk/protected/views/personagem/_form_mentor.php
@@ -23,21 +23,21 @@
 						<a class="dcontexto">
 							<label><img src="<?php echo Yii::app()->request->baseUrl.'/assets/web/img/mentor/'.$mentores[0].'.jpg'; ?>" width="170" height="250"><br>
 							<?php echo $form->radioButton($model, 'id_mentor', array('value'=>$mentores[0],'uncheckValue'=>null)); ?></label>
-							<span>Informações de cada mentor e blabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla bla</span>
+							<span>Aerys Targaryen</span>
 						</a>
 					</td>
 					<td>
 						<a class="dcontexto">
 							<label><img src="<?php echo Yii::app()->request->baseUrl.'/assets/web/img/mentor/'.$mentores[1].'.jpg'; ?>" width="170" height="250"><br>
 							<?php echo $form->radioButton($model, 'id_mentor', array('value'=>$mentores[1],'uncheckValue'=>null)); ?></label>
-							<span>Informações de cada mentor e blabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla bla</span>
+							<span>Daenerys Targaryen</span>
 						</a>
 					</td>
 					<td>
 						<a class="dcontexto">
 							<label><img src="<?php echo Yii::app()->request->baseUrl.'/assets/web/img/mentor/'.$mentores[2].'.jpg'; ?>" width="170" height="250"><br>
 							<?php echo $form->radioButton($model, 'id_mentor', array('value'=>$mentores[2],'uncheckValue'=>null)); ?></label>
-							<span>Informações de cada mentor e blabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla blablabla bla bla</span>
+							<span>Vyserys Targaryen</span>
 						</a>
 					</td>
 				</tr>
