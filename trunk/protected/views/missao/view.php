65e3059472b70863296c4017fa194012c0f17f90
rickinbsb@gmail.com <rickinbsb@gmail.com@d6a40b73-17b3-d587-71e3-4df09bc2e9da>
2013-12-11 19:32:03 +0000

    correção da gambiarra do mercado

--- trunk/protected/views/missao/view.php
+++ trunk/protected/views/missao/view.php
@@ -1,17 +1,7 @@
 <?php
 /* @var $this MissaoController */
 /* @var $model Missao */
-$dbcon = new PDO('pgsql:host=localhost;dbname=got', 'got', 'got');
-$usuario = Yii::app()->user->id;
-$xp = 4;
-$gold = 540;
-
-
-$sql = "UPDATE personagem
-        SET xp_personagem=?, gold_personagem=?
-        WHERE id_personagem=?";
-$q = $dbcon->prepare($sql);
-$q->execute(array($xp,$gold,$usuario));
+$model->iniciaMissao();
 
 ?>
 
@@ -23,7 +13,7 @@ $q->execute(array($xp,$gold,$usuario));
             <div class="missao-content">
                 <img src="<?php echo Yii::app()->request->baseUrl.'/assets/web/img/Summerhall.jpg' ?>" width="100" height="100">
                 <label>Missão "<?php echo $model->nome_missao ?>" em andamento!</label>
-                <label id="btn"><a href="<?php echo Yii::app()->request->baseUrl.'/missa0/1' ?>"><button>Atualizar</button></a></label>
+                <label id="btn"><a href="<?php echo Yii::app()->request->baseUrl.'/missao' ?>"><button>Atualizar</button></a></label>
             </div>
     </div>
 </div>
